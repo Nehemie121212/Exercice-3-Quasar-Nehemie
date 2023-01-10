f544b6766e92779570a366a0e7504170db933c9d
zahrneh <nehemie.zahrane@gmail.com>
2023-01-10 14:59:30 +0100

    base

--- src/components/PlatComponent.vue
+++ src/components/PlatComponent.vue
@@ -1,81 +1,57 @@
 <template>
-<q-card
-  class="card">
-  <q-img
-    :src="plat.image ? plat.image : 'statics/image-placeholder.png'"
-    basic
-    contain
-  >
-    <div class="absolute-bottom text-h6">
-      {{ plat.nom }}
-    </div>
-  </q-img>
+  <q-card
+    class="card">
+    <q-img
+      :src="plat.image"
+      basic
+      contain
+    >
+      <div class="absolute-bottom text-h6">
+        {{ plat.nom }}
+      </div>
+    </q-img>
 
-  <q-card-section>
-    <q-rating
-      readonly
-      :model-value="plat.note"
-      size="2em"
-      color="orange"
-      class="q-mt-sm"
-    />
-  </q-card-section>
+    <q-card-section>
+      <q-rating
+        readonly
+        :model-value="plat.note"
+        size="2em"
+        color="orange"
+        class="q-mt-sm"
+      />
+    </q-card-section>
 
-  <q-card-section class="description">
-    <span v-if="plat.description">{{ plat.description }}</span>
-    <i v-else>Aucune descripion fournie.</i>
-  </q-card-section>
+    <q-card-section class="description">
+      {{ plat.description }}
+    </q-card-section>
 
-  <q-card-actions
-    class="absolute-bottom"
-    align="right">
-    <q-btn
-      @click="afficherFormPlat = true"
-      icon="edit"
-      color="blue"
-      flat>Modifier</q-btn>
-    <q-btn
-      @click="dialogSupprimer()"
-      icon="delete"
-      color="red"
-      flat>Supprimer</q-btn>
-  </q-card-actions>
+    <q-card-actions
+      class="absolute-bottom"
+      align="right">
+      <q-btn
+        @click="afficherFormPlat = true"
+        icon="edit"
+        color="blue"
+        flat>Modifier</q-btn>
+      <q-btn
+        icon="delete"
+        color="red"
+        flat>Supprimer</q-btn>
+    </q-card-actions>
 
-  <q-dialog
-    v-model="afficherFormPlat">
-    <form-plat action="modifier" :platAModifier="plat" @close="afficherFormPlat = false"  />
-  </q-dialog>
-</q-card>
+    <q-dialog
+      v-model="afficherFormPlat">
+      <form-plat action="modifier" />
+    </q-dialog>
+  </q-card>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
 export default {
   props: ['plat'],
   data () {
     return {
       afficherFormPlat: false
-    }
-  },
-  methods: {
-    ...mapActions('plats', ['supprimerPlat']),
-    dialogSupprimer () {
-      this.$q.dialog({
-        title: 'Supprimer plat',
-        message: 'Etes vous sur de vouloir supprimer ce plat ?',
-        persistent: true,
-        ok: {
-          label: 'Supprimer',
-          push: true
-        },
-        cancel: {
-          label: 'Annuler',
-          push: true,
-          color: 'grey'
-        }
-      }).onOk(() => {
-        this.supprimerPlat(this.plat.id)
-      })
     }
   },
   components: {
